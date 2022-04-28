46734b00a82a27a24e9c7abc1858815ad2027e58
Dsh <by.dsh116@gmail.com>
2022-04-28 09:49:25 +0300

    fix note length, add scroll. placed the inputs in the center.

--- src/App.js
+++ src/App.js
@@ -1,59 +1,56 @@
-import './App.css';
-import style from './styles/styles.module.css'
-import React from 'react'
-import { InputGroup } from './components/InputGroup'
+import "./App.css";
+import style from "./styles/styles.module.css";
+import React from "react";
+import { InputGroup } from "./components/InputGroup";
 // import { Button } from './components/Button';
-
 
 class App extends React.Component {
   state = {
-    notes: []
-  }
+    notes: [],
+  };
   updateParentComponentState = (value) => {
     this.setState({
-      notes: [...this.state.notes, value]
-    })
-  }
+      notes: [...this.state.notes, value],
+    });
+  };
 
-  
-  deletePost (id) {
-    this.setState(({notes})=> {
-      const copNotes = [...notes]
-      copNotes.splice(id, 1)
-      return {notes: copNotes}
-    })
+  deletePost(id) {
+    this.setState(({ notes }) => {
+      const copNotes = [...notes];
+      copNotes.splice(id, 1);
+      return { notes: copNotes };
+    });
   }
-
 
   render() {
     return (
-      <div className="App" >
+      <div className="App">
         <InputGroup onChange={this.updateParentComponentState} />
         <div className={style.notesBox}>
           {this.state.notes.map((note, id) => {
-
             return (
-              <div 
-              onClick={() => this.deletePost(id)}
-              key={id} className={style.notes}>
-              <strong>{note.inputTitle}</strong>
-              <br/>
-              <hr style={{ backgroundColor: "#b7b1c9", height:'2px'}}/>
-              <span>{note.inputText}</span>
-              <button className={style.button}
-              onClick={this.deletePost}
-               >
-                    Delete
-                </button> 
-               </div>
-            )
-           })}
+              <div
+                onClick={() => this.deletePost(id)}
+                key={id}
+                className={style.notes}
+              >
+                <strong>{note.inputTitle}</strong>
+                <br />
+                <hr style={{ backgroundColor: "#b7b1c9", height: "2px" }} />
+                <span>{note.inputText}</span>
+                <button
+                  className={`${style.button} ${style.delete}`}
+                  onClick={this.deletePost}
+                >
+                  Delete
+                </button>
+              </div>
+            );
+          })}
         </div>
       </div>
-    )
-  };
+    );
+  }
 }
 
 export default App;
-
-
--- src/components/InputGroup.jsx
+++ src/components/InputGroup.jsx
@@ -1,44 +1,54 @@
-import React from 'react'
-import styles from '../styles/styles.module.css'
+import React from "react";
+import style from "../styles/styles.module.css";
 // import { Button } from './Button'
 
-
 export class InputGroup extends React.Component {
-    state = {
-        inputTitle: '',
-        inputText: ''
+  state = {
+    inputTitle: "",
+    inputText: "",
+  };
+
+  onClick = () => {
+    if (!(this.state.inputText && this.state.inputTitle)) {
+      alert("Both of fields must be fulfilled");
+      return;
     }
 
-    onClick = () => {
-        const noteObject = {
-            inputTitle: this.state.inputTitle,
-            inputText: this.state.inputText,
-        }
-        this.props.onChange(noteObject)
-    }
+    const noteObject = {
+      inputTitle: this.state.inputTitle,
+      inputText: this.state.inputText,
+    };
+    this.props.onChange(noteObject);
+  };
 
-    render() {
-        return (
-            <div>
-                <input
-                 name='title'
-                    type="text"
-                    placeholder='Add title'
-                    className={styles.noteInput}
-                    onChange={(e) => { this.setState({ inputTitle: e.target.value }) }} />
+  render() {
+    return (
+      <div className={style.inputDiv}>
+        <input
+          name="title"
+          type="text"
+          placeholder="Add title"
+          className={style.noteInput}
+          onChange={(e) => {
+            this.setState({ inputTitle: e.target.value });
+          }}
+        />
 
-                <input
-                    className={styles.noteInput}
-                    type="text" placeholder='add your thought'
-                    onChange={(e) => {
-                        this.setState({ inputText: e.target.value })
-                    }} />
-                <button className={styles.button} onClick={this.onClick} >
-                    Add on page
-                </button>
-                
-            </div>
-        )
-    }
+        <input
+          className={style.noteInput}
+          type="text"
+          placeholder="add your thought"
+          onChange={(e) => {
+            this.setState({ inputText: e.target.value });
+          }}
+        />
+        <button
+          className={`${style.button} ${style.submit}`}
+          onClick={this.onClick}
+        >
+          Add on page
+        </button>
+      </div>
+    );
+  }
 }
-
