c65ccf6966b40b35509eedccd5ac5c861b729348
byDsh116 <by.dsh116@gmail.com>
2022-05-26 13:38:58 +0300

    slightly fixed textarea styles

--- src/App.js
+++ src/App.js
@@ -16,6 +16,15 @@ class App extends React.Component {
     currentNote: null,
   };
 
+  componentDidMount() {
+    // document.addEventListener('click', this.handlePageClick);
+    window.addEventListener('click', this.handlePageClick);
+  }
+
+  handlePageClick(e){
+    e.stopPropagation()
+  }
+
   updateParentComponentState = (value) => {
     this.setState({
       notes: [...this.state.notes, value],
@@ -48,71 +57,80 @@ class App extends React.Component {
     });
   };
 
-editNote(id, body, title){
-  const currentNotes = this.state.notes
-  if(!currentNotes.length){
-    console.log('error: state is empty')
-    return
-  }
-  const updatedNotes = currentNotes.map((note) => {
-    console.log(note)
-    if(note.id === id){
-      return {id: id, inputTitle: title, inputText: body}
+  editNote(id, body, title) {
+    const currentNotes = this.state.notes;
+    if (!currentNotes.length) {
+      console.log("error: state is empty");
+      return;
     }
-    return note
-  })
-  console.log(currentNotes, updatedNotes)
-  this.setState({notes: updatedNotes})
-}
+    const updatedNotes = currentNotes.map((note) => {
+      console.log(note);
+      if (note.id === id) {
+        return { id: id, inputTitle: title, inputText: body };
+      }
+      return note;
+    });
+    console.log(currentNotes, updatedNotes);
+    this.setState({ notes: updatedNotes });
+  }
 
-trashIcon = (
-  <svg
-  xmlns="http://www.w3.org/2000/svg"
-  width="16"
-  height="16"
-  fill="currentColor"
-  class="bi bi-trash"
-  viewBox="0 0 16 16"
-  >
+  trashIcon = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      fill="currentColor"
+      className="bi bi-trash"
+      viewBox="0 0 16 16"
+    >
       <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
       <path
-        fill-rule="evenodd"
+        fillRule="evenodd"
         d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
-        />
+      />
     </svg>
   );
-  
+
   noteDate = (
     <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="16"
-    height="16"
-    fill="currentColor"
-    class="bi bi-calendar3"
-    viewBox="0 0 16 16"
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      fill="currentColor"
+      className="bi bi-calendar3"
+      viewBox="0 0 16 16"
     >
       <path d="M14 0H2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM1 3.857C1 3.384 1.448 3 2 3h12c.552 0 1 .384 1 .857v10.286c0 .473-.448.857-1 .857H2c-.552 0-1-.384-1-.857V3.857z" />
       <path d="M6.5 7a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm-9 3a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm-9 3a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
     </svg>
   );
-  
-  render() {
 
+  render() {
     return (
-      <div className="App" onClick={(e) => {if ( this.state.modalActive ) {
-        this.setState({modalActive: false})
-      };}}>
+      //////////////////////////////////////
+      <div
+        className="App"
+        // onClick={(e) => {
+        //   if (this.state.modalActive) {
+        //     this.setState({ modalActive: false })
+        //   }
+        // }}
+      >
+        {/* /////////////////////////////// */}
         <InputGroup onChange={this.updateParentComponentState} />
+        {this.state.modalActive &&
         <ModalWindow
           isModalActive={this.state.modalActive}
           currentNote={this.state.currentNote}
           editNote={(id, title, body) => this.editNote(id, title, body)}
-          hideModal={()=>{this.setState({modalActive: false})}}
-          />
-
+          hideModal={() => {
+            this.setState({ modalActive: false });
+          }}
+        />
+        }
         <div className={style.notesBox}>
           {this.state.notes.map((note) => {
-            console.log(note, this.state.notes)
+            console.log(note, this.state.notes);
             return (
               <div
                 key={note.id}
@@ -143,7 +161,10 @@ trashIcon = (
                 <div className={style.deleteDiv}>
                   <button
                     className={style.delete}
-                    onClick={() => this.deletePost(note.id)}
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      this.deletePost(note.id)
+                    }}
                   >
                     {this.trashIcon}
                   </button>
